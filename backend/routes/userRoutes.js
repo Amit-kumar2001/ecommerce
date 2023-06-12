453bbcefd1f688e674e0d6d50471ffde4da8bc1e
Amit-kumar2001 <asgamer8504@gmail.com>
2023-06-12 09:49:52 +0530

    Initial commit

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,29 @@
+import express from 'express'
+const router = express.Router()
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updatetUserProfile,
+  getUsers,
+  deleteUser,
+  getUserByID,
+  updateUser,
+} from '../controllers/userController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
+
+router.route('/').post(registerUser).get(protect, admin, getUsers)
+router.post('/logout', logoutUser)
+router.post('/auth', authUser)
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updatetUserProfile)
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserByID)
+  .put(protect, admin, updateUser)
+
+export default router
